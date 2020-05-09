66a3c19c5ae822295a037cf039ff5e27da31414b
Tom McKinney <tom@upstreamcode.com>
2020-05-09 13:26:25 -0400

    WIP reworking card layout

--- src/web/components/Card/index.tsx
+++ src/web/components/Card/index.tsx
@@ -9,8 +9,10 @@ const cardWidth = 2.5;
 const cardGutterWidth = 0.45;
 
 const cardColor = ({ color, theme }: ThemeProps<DefaultTheme> & CardInfo) => (color === 'red' ? theme.card.red : theme.card.black);
+const cardSuitSymbol = ({ suitSymbol }: ThemeProps<DefaultTheme> & CardInfo) => suitSymbol;
 
 const CardWrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -22,12 +24,21 @@ const CardWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0 2px 2px ${({ theme }) => theme.colors.grey[900]};
 
+  &:before {
+    content: '${cardSuitSymbol}';
+    color: ${cardColor};
+    font-size: 50px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
   * {
     color: ${cardColor};
   }
 
   .portrait {
-    border: 1px solid red;
+    border: 2px solid ${cardColor};
     height: 100%;
     width: 100%;
   }
@@ -44,4 +55,62 @@ const Card: React.SFC<Props> = ({ card }) => (
   </CardWrapper>
 );
 
-export default Card;
+
+const NewCardWraper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  justify-content: space-between;
+  height: ${cardHeight * 100}px;
+  width: ${cardWidth * 100}px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0 2px 2px ${({ theme }) => theme.colors.grey[900]};
+
+  * {
+    color: ${cardColor};
+  }
+
+  .gutter {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    flex: 0 0 ${cardGutterWidth * 100}px;
+    padding: 10px 0;
+
+    .value {
+      font-size: 30px;
+    }
+
+    .suit {
+      font-size: 30px;
+      line-height: 20px
+    }
+
+    &:last-child {
+      flex-flow: column-reverse;
+
+      .value,
+      .suit {
+        transform: rotate(180deg);
+      }
+    }
+  }
+`;
+
+const NewCard: React.FC<Props> = ({ card }) => (
+  <NewCardWraper {...card}>
+    <div className="gutter">
+      <span className="value">{card.value}</span>
+      <span className="suit">{card.suitSymbol}</span>
+    </div>
+    <div className="portrait" />
+    <div className="gutter">
+      <span className="value">{card.value}</span>
+      <span className="suit">{card.suitSymbol}</span>
+    </div>
+  </NewCardWraper>
+);
+
+export default NewCard;
--- src/web/pages/games/[gameId].tsx
+++ src/web/pages/games/[gameId].tsx
@@ -1,10 +1,10 @@
 import { NextPage, GetServerSideProps } from 'next';
+import styled from 'styled-components';
 import fetch from 'isomorphic-fetch';
 import Title from '../../components/Title';
 import Options from '../../components/Options';
 import Card from '../../components/Card';
 import getCard from '../../utils/getCard';
-import styled from 'styled-components';
 
 type GamePageProps = {
   game: Game;
@@ -15,23 +15,38 @@ const GameBoard = styled.div`
   flex-direction: row;
 `;
 
-const GamePage: NextPage<GamePageProps> = ({ game }) => (
-  <div>
-    <Title>{game.name}</Title>
-    <GameBoard>
-      <Card card={getCard(game.current_card)} />
-      <Options options={game.options} />
-    </GameBoard>
-  </div>
-);
+const GamePage: NextPage<GamePageProps> = ({ game }) => {
+  if (game == null) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Title>{game.name}</Title>
+      <GameBoard>
+        <Card card={getCard(game.current_card)} />
+        <Options options={game.options} />
+      </GameBoard>
+    </div>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<GamePageProps> = async (context) => {
   const { gameId } = context.query;
   const response = await fetch(`http://127.0.0.1:8000/bus/game/${gameId}`);
-  const gameData = await response.json() as Game;
+
+  if (response.ok) {
+    const gameData = await response.json() as Game;
+    return {
+      props: {
+        game: gameData,
+      },
+    };
+  }
+
   return {
     props: {
-      game: gameData,
+      game: null as Game,
     },
   };
 };
